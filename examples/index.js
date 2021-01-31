1f25ff6076d253756f884d259a431ee0a15c9547
lilindog <lilin@lilin.site>
2021-01-31 12:51:47 +0800

    增加table方法

--- examples/index.js
+++ examples/index.js
@@ -4,23 +4,32 @@ const jr = new JR128643A({ path: "COM5" });
 
 !async function () {
     
-    console.log(await jr.handShake());
-    console.log(await jr.clearScreen());
-    console.log(await jr.text("hello lilindog!"));
-    await sleep(1000);
+    while (true) {
+    await jr.clearScreen();
+    await jr.table([
+        ["cpu使用率:", "    24%"],
+        ["cpu空闲:", "    28%"],
+        ["内存使用:", "    50%"],
+        ["内存空闲:", "    50%"],
+    ]);
+    await sleep(2000);
+    await jr.clearScreen();
+    await jr.text("您当前的IP无法定位到具体城市，建议使用“城市名称+天气”重新搜索");
+    await sleep(2000);
+}
 
-    setInterval(() => {
-        jr.clearScreen();
-        let 
-        d = new Date(),
-        str = d.getFullYear() + "-";
-        str += (d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1) + "-";
-        str += (d.getDate() < 10 ? "0" + d.getDate() : d.getDate()) + " ";
-        str += (d.getHours() < 10 ? "0" + d.getHours() : d.getHours()) + ":";
-        str += (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + ":";
-        str += d.getSeconds() <10 ? "0" + d.getSeconds() : d.getSeconds();
-        jr.text(str);
-    }, 1000);
+    // setInterval(() => {
+    //     jr.clearScreen();
+    //     let 
+    //     d = new Date(),
+    //     str = d.getFullYear() + "-";
+    //     str += (d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1) + "-";
+    //     str += (d.getDate() < 10 ? "0" + d.getDate() : d.getDate()) + " ";
+    //     str += (d.getHours() < 10 ? "0" + d.getHours() : d.getHours()) + ":";
+    //     str += (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + ":";
+    //     str += d.getSeconds() <10 ? "0" + d.getSeconds() : d.getSeconds();
+    //     jr.text("现在时间:         " + str);
+    // }, 1000);
 
 }();
 
--- src/index.js
+++ src/index.js
@@ -8,16 +8,34 @@
  const iconv = require("iconv-lite");
 
  module.exports = class JR128643A {
-     static fontSizes = {
+
+    /**
+     * 可选字号 
+     * 详见吉润文档
+     */
+    static fontSizes = {
         1: 0x31,
         2: 0x32,
         3: 0x33,
         4: 0x34
+    }
+
+     /**
+      * 各种字号的宽高 
+      * 详见吉润协议文档
+      */
+     static fontSizeMap = {
+        [Object.keys(JR128643A.fontSizes)[0]]: 12,
+        [Object.keys(JR128643A.fontSizes)[1]]: 16,
+        [Object.keys(JR128643A.fontSizes)[2]]: 24,
+        [Object.keys(JR128643A.fontSizes)[3]]: 32,
      }
      static commandHeader = [0xfe, 0xfd];
      static commandEnd = [0xdd, 0xcc, 0xbb, 0xaa];
      static defaultBaudRate = 115200;
-     static defaultEncoding = "utf8";
+     static defaultEncoding = "GB2312";
+     static height = 64;
+     static width = 128;
      constructor ({ path, baudRate  = JR128643A.defaultBaudRate }) {
         this.path = path, this.baudRate = baudRate;
         this._buf = Buffer.alloc(0);
@@ -43,7 +61,27 @@
      }
 
      /**
+      * 计算字符点数
+      * 
+      * @param  {Array} text
+      * @return {Number} 
+      */
+     _computeTextDot (texts = [], size = Object.keys(JR128643A.fontSizes)[0]) {
+        let len = 0;
+        texts.forEach(text => {
+            const 
+            halfAngle = text.match(/[\x00-\xff]/g),
+            fullAngle = text.match(/^[\x00-\xff]/g);
+            len += halfAngle ? halfAngle.length * JR128643A.fontSizeMap[size] / 2 : 0;
+            len += fullAngle ? fullAngle.length * JR128643A.fontSizeMap[size] : 0;
+        });
+        return Math.floor(len);
+     }
+
+     /**
       * 握手，主要用于检测屏幕是否有响应 
+      * 
+      * @return {Boolean}
       */
      async handShake () {
         const res = await this._sp.write([0x11, 0x00, 0x00], true);
@@ -58,10 +96,34 @@
      }
 
      /**
-      * 写入文字, 过长自动换行
+      * 写入表格 
+      * 
+      * @param  {Array} table
+      * @return {JR128643A}
       */
-     async text (text = "", size = JR128643A.fontSizes[1]) {
+    async table (table = [[]], size = Object.keys(JR128643A.fontSizes)[0]) {
+        if (table.length * JR128643A.fontSizeMap[size] > JR128643A.height) throw `行数溢出! 当前size建议行数：${Math.floor(JR128643A.height / JR128643A.fontSizeMap[size])} 行！`;
+        table.forEach((row, rowIndex) => {
+            const y = rowIndex * JR128643A.fontSizeMap[size], cellDot = JR128643A.width / row.length;
+            row.forEach((text, textIndex) => {
+                const textBuf = iconv.encode(text, JR128643A.defaultEncoding);
+                const hexs = [
+                    JR128643A.fontSizes[size], 
+                    textBuf.byteLength + 4 >> 8, textBuf.byteLength + 4, 
+                    cellDot * textIndex >> 8, cellDot * textIndex,
+                    y >> 8, y,
+                    ...textBuf
+                ];
+                this._sp.write(hexs);
+            });
+        });
+    }
+
+    /**
+     * 写入文字, 过长自动换行
+     */
+    async text (text = "", size = Object.keys(JR128643A.fontSizes)[0]) {
         let buf = iconv.encode(text, JR128643A.defaultEncoding);
-        return await this._sp.write([size, buf.byteLength + 4 >> 8, buf.byteLength + 4, 0x00, 0x00, 0x00, 0x00, ...buf]);
-     }
- }
+        return await this._sp.write([JR128643A.fontSizes[size], buf.byteLength + 4 >> 8, buf.byteLength + 4, 0x00, 0x00, 0x00, 0x00, ...buf]);
+    }
+}
